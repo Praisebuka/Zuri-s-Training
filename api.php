621aae5db8f8170b96dc0d13890e5e38c01dae19
Praise Ebuka <109698690+Praisebuka@users.noreply.github.com>
2023-09-09 15:04:36 +0100

    Update and rename index.php to api.php

---
+++ api.php
@@ -0,0 +1,29 @@
+
+date_default_timezone_set('UTC');
+header('Content-Type: application/json');
+if (isset($_GET['slack_name']) && isset($_GET['track'])) {
+    $slackName = $_GET['slack_name'];
+    $track = $_GET['track'];
+ 
+    $githubFileURL = 'https://github.com/Praisebuka/Zuri-s-Training/edit/main/api.php';
+    $githubSourceURL = 'https://github.com/dayveed12345/hng-intern';
+
+    $utcDateTime = gmdate('Y-m-d\TH:i:s\Z');
+    $currentDay = date('l');
+    $response = [
+        'slack_name' => $slackName,
+        'day_
+_of_week' => $currentDay,
+        'utc_time' => $utcDateTime,
+        'track' => $track,
+        'github_file_url' => $githubFileURL,
+        'github_source_url' => $githubSourceURL,
+        'status_code' => 200,
+    ];
+
+    echo json_encode($response);
+} else {
+
+    echo json_encode(['error' => 'Missing required parameters']);
+    http_response_code(400);
+}
